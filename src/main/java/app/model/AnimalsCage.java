61585abb6371084889294a8b710491af85b56481
gtboga <122990173+gtboga@users.noreply.github.com>
2024-03-17 16:10:05 +0300

    Мой первый commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private final Timer timer;
+
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +24,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
